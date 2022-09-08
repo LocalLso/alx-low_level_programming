6f76972391d42b95725d8d87e57d86c5e59c2665
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-08 22:51:34 +0000

    C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return: Always 0(Succes)
+ * Return: Always 0 (Success)
  */
-int main ()
+int main()
 {
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
-	int intType;
-	char charType;
+/* C program that prints the size of various types on the computer it is compiled and run on.*/
+	printf("Size of a char: %i byte(s)\n ",sizeof(char));
+	printf("Size of an int: %i byte(s)\n ",sizeof(int));
+	printf("Size of a long int: %i byte(s)\n ",sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n ",sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n ",sizeof(float));
 
-	/*size of evaluates the size of a variable*/
-	printf("Size of a char: %i byte(s)\n", sizeof(charType));
-	printf("Size of an int: %i byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %i byte(s)\n", sizeof(longintType));
-	print("Size of a long long int: %i byte(s)\n", sizeof(longlongintType));
-	printf("Size of float: %i byte(s)\n", sizeof(floatType));
-
-	return 0;
+	return (0);
 }
