d29d670e0f719133cd96d70d8fb3869e7f74f6d7
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-09 23:27:42 +0000

    C program that prints lowercase alphabets but excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*C program prints alphabets but exclude e and q in lowercase*/
+	/*initialize*/
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch == 'e')
+		{
+			ch = 'f';
+		}
+		else if (ch == 'q')
+		{
+			ch = 'r';
+		}	
+		putchar(ch);
+	}
+
+	/*prints a newline*/
+	putchar('\n');
+
+	return (0);
+}
