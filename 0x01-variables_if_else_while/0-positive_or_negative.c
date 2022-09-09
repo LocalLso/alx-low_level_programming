2eafa8c5b58b13eb647b0e0215f4ba9d809e3971
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-09 16:52:36 +0000

    A program that assigns a random number to a variable n each time is executed

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* main -Entry point*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+/*This program assigns a random number to a variable n*/
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d: is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("%d: is zero\n");
+	}
+	else
+	{
+		printf("%d: is negative");
+	}
+
+	return (0);
+}
