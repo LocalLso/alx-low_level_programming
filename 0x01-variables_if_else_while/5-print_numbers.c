d2466e7d89e91e95d960656f70a8a0531d0bf77c
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-09 23:42:08 +0000

    C program that prints all single digits numbers of base 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*C program that prints all single base 10 digits*/
+	/*initialize variables*/
+	int singleDigNum;
+
+	for (singleDigNum = 0 ; singleDigNum < 10 ; singleDigNum++)
+	{
+		printf("%d", singleDigNum);
+	}
+
+	return (0);
+}
