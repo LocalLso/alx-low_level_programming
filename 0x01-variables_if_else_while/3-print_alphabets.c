19f0c43e57cd8323851f002cf8395a8e918ef661
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-09 22:21:09 +0000

    A programm that prints both upper and lower case letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*C program to print both upper and lower case alphabets*/
+	/*initialize*/
+	char gothrough;
+	char gothrough1;
+
+	/*print out characters from the initial value of gothrough*/
+	for (gothrough = 'a' ; gothrough <= 'z' ; gothrough++)
+	{
+		for (gothrough1 = 'A' ; gothrough1 <= 'Z' ; gothrough++)
+		{
+			putchar(gothrough);
+			putchar(gothrough1);
+		}
+	}
+
+	/*prints out the newline*/
+	putchar('\n');
+
+	return (0);
+}
