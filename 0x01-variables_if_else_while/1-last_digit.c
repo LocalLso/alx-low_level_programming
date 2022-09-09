082f5c4ca58839abd0adb291eb9ea84b0b20ea16
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-09 19:57:59 +0000

    A C program that assigns random number to a variable n and checks the last digit of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,23 @@ int main(void)
 {
 /*C progam that assigns random number a variable n*/
 	int n;
-	int lastDigit;
+	int lastDig;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	lastDig = n % 10;
 
-	if (lastDigit > 5)
+	if (lastDig > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDig);
 	}
-	else if (lastDigit == 0)
+	else if (lastDig == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastDig);
 	}
-	else if (lastDigit < 6 && lastDigit != 0)
+	else if (lastDig < 6 && lastDig != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDig);
 	}
 
 	return (0);
