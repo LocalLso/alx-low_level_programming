a66542a5146c24e5becea299f5b2775607d1a197
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-10 00:01:01 +0000

    C program that prints single digits numbers in base 10 using putchar starting at 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry poin
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+/*C program that prints single digit numbers of base 10 using putchar*/
+	/*inititialize variable*/
+	int ch;
+
+	for (ch = 0 ; ch < 10 ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
