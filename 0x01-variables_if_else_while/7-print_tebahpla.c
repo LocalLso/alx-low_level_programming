29553e4fe7ec8b6650f6d132f92cebda8a9e2118
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-10 01:11:24 +0000

    C program that prints alphabets in reverse using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+/*C program that prints alphabets in reverse, in lowercase letter*/
+	/*initialize variables*/
+	char reverse;
+
+	for (reverse = 'z' ; reverse <= 'a' ; reverse++)
+	{
+		putchar(reverse);
+	}
+
+	putchar('\n');
+	return (0);
+}
