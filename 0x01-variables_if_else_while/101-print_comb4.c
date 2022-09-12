6d4226bdab54529e398faa3e4a2ebe351d171f9f
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-12 10:05:33 +0000

    C program that prints all possible combinations of three different digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*C program that prints all different combinations of three digits*/
+	/*initialize variaables*/
+	int d1;
+	int d2;
+	int d3;
+
+	for (d1 = 0 ; d1 <= 7 ; d1++)
+	{
+		for (d2 = 1 ; d2 <= 8 ; d2++)
+		{
+			for (d3 = 2 ; d3 <= 9 ; d3++)
+			{
+				if (d1 == d2 == d3)
+				{
+					continue;
+				}
+				else if (d1 > d2 > d3)
+				{
+					continue;
+				}
+				else
+				{
+					putchar((d1 % 10) + '0');
+					puthcar((d2 % 10) + '0');
+					putchar((d3 % 10) + '0');
+						if (d1 == 7 && d2 == 8 && d3 == 9)
+						{
+							continue;
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
