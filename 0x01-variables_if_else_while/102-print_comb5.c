0ae0cd76cbbd0abccb53e189ab1c1fe58711029e
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-12 12:24:37 +0000

    C program that prints all possible different combinations of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,13 +12,13 @@ int main(void)
 	{
 		for (d2 = 0 ; d2 <= 8 ; d2++)
 		{
-			putchar((d1 % 10) + '0');
-			putchar((d2 % 10) + '0');
-			putchar(' ');
 		for (d3 = 0 ; d3 <= 9 ; d3++)
 		{
 		for (d4 = 1 ; d4 <= 9 ; d4++)
 		{
+			putchar((d1 % 10) + '0');
+			putchar((d2 % 10) + '0');
+			putchar(' ');
 			putchar((d3 % 10) + '0');
 			putchar((d4 % 10) + '0');
 			if (d1 == 9 && d2 == 8 && d3 == 9 && d4 == 9)
