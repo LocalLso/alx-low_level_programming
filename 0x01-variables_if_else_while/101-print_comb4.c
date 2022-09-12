55cbc785541617e3707341a8d9ac220057491db7
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-12 11:27:22 +0000

    C program that prints all possible combinations of three different digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,9 +6,7 @@
  */
 int main(void)
 {
-	int d1;
-	int d2;
-	int d3;
+	int d1, int d2, int d3;
 
 	for (d1 = 0 ; d1 <= 7 ; d1++)
 	{
@@ -16,32 +14,32 @@ int main(void)
 		{
 			for (d3 = 2 ; d3 <= 9 ; d3++)
 			{
-				if (d1 == d2 && d1 == d3 && d2 == d3)
-				{
-					continue;
-				}
-				else if (d1 == d2 || d2 == d3)
-				{
-					continue;
-				}
-				else if (d1 > d2 || d1 > d3 || d2 > d3)
-				{
-					continue;
-				}
-				else
-				{
-					putchar((d1 % 10) + '0');
-					putchar((d2 % 10) + '0');
-					putchar((d3 % 10) + '0');
-						if (d1 == 7 && d2 == 8 && d3 == 9)
-						{
-							continue;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+			if (d1 == d2 && d1 == d3 && d2 == d3)
+			{
+				continue;
+			}
+			else if (d1 == d2 || d2 == d3)
+			{
+				continue;
+			}
+			else if (d1 > d2 || d1 > d3 || d2 > d3)
+			{
+				continue;
+			}
+			else
+			{
+				putchar((d1 % 10) + '0');
+				putchar((d2 % 10) + '0');
+				putchar((d3 % 10) + '0');
+					if (d1 == 7 && d2 == 8 && d3 == 9)
+					{
+						continue;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
