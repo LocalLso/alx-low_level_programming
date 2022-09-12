da880e5f884e0e053e184f8b441432fb9e6f7ed0
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-12 07:42:22 +0000

    C program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,12 @@ int main(void)
 	for (d1 = 0 ; d1 <= 8 ; d1++)
 	{
 		for (d2 = 1 ; d2 <= 9 ; d2++)
-		{	
+		{
 			if (d1 == d2 || d2 == d1)
+			{
+				continue;
+			}
+			else if (d1 == d2 && d2 == d1)
 			{
 				continue;
 			}
@@ -26,40 +30,6 @@ int main(void)
 				putchar(' ');
 			}
 		}
-
-		/*else if (d1 == 1)
-		for (d2 = 2 ; d2 <= 9 ; d2++)
-			continue;
-
-		else if (d1 == 2)
-		for (d2 = 3 ; d2 <= 9 ; d2++)
-			continue;
-
-		else if (d1 == 3)
-		for (d2 = 4 ; d2 <= 9 ; d2++)
-			continue;
-
-		else if (d1 == 4)
-		for (d2 = 5 ; d2 <= 9 ; d2++)
-			continue;
-
-		else if (d1 == 5)
-		for (d2 = 6 ; d2 <= 9 ; d2++)
-			continue;
-
-		else if (d1 == 6)
-		for (d2 = 7 ; d2 <= 9 ; d2++)
-			continue;
-
-		else if (d1 == 7)
-		for (d2 = 8 ; d2 <= 9 ; d2++)
-			continue;
-
-		else if (d1 == 8)
-		for (d2 = 9 ; d2 <= 9 ; d2++)
-			continue;
-
-		*/
 	}
 	putchar('\n');
 	return (0);
