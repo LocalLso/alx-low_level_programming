5f742720bcf9e25d2fdddddf4a8dc81a85da039f
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-13 12:08:00 +0000

    C program that prints a string using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * C program that prints _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = '_putchar';
+	puthcar(ch);
+
+	return (0);
+}
