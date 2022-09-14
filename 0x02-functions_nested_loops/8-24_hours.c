633aa5e6c2f010770a5676e068a2aba503682098
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-14 17:46:16 +0000

    C program that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 	{
 	for (l = 0; l <= 9; l++)
 	{
-		if ((i == 2 && j > 3) && (k > 0 || l > 0) && (k == 0 && l == 0))
+		if ((i == 2 && j > 3) && (k > 0 || l > 0) || (k == 0 && l == 0))
 		{
 			continue;
 		}
