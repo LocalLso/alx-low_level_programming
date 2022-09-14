782396707f5971ca5b1417ac2827b84daca6653b
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-14 12:03:54 +0000

    C program that computes the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *@ld: Character to print
+ * print_last_digit - prints last digit of a number
+ *
+ * Return: Last digit
+ */
+int print_last_digit(int ld)
+{
+	return (ld % 10);
+}
