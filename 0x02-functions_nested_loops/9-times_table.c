4ed4a4384619026cde7f28000dfedb47416ab268
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-14 22:09:37 +0000

    C program that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,6 +17,11 @@ void times_table(void)
 	for (n = 0; n <= 9; n++)
 	{
 		mul = m * n;
+		if (n != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 		if (n == 9)
 		{
 			if (mul < 10)
@@ -31,14 +36,11 @@ void times_table(void)
 		}
 		else if (mul < 10)
 		{
-			_putchar('0' + mul);
-			_putchar(',');
 			_putchar(' ');
+			_putchar('0' + (mul % 10));
 		}
 		else
 		{
-			_putchar(',');
-			_putchar(' ');
 			_putchar((mul / 10) + '0');
 			_putchar((mul % 10) + '0');
 		}
