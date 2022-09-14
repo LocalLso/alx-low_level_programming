cfda9dd8c16dbb30c05fc4951abf10bb493c1fab
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-14 12:38:00 +0000

    C program that computes the last digit of a number

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -14,9 +14,12 @@ int main(void)
 
 	r = print_last_digit(98);
 	_putchar('0' + r);
+	_putchar('0' + r);
 	r = print_last_digit(0);
 	_putchar('0' + r);
+	_putchar('0' + r);
 	r = print_last_digit(-1024);
+	_putchar('0' + r);
 	_putchar('0' + r);
 	_putchar('\n');
 	return (0);
