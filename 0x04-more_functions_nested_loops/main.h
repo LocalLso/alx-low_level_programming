28838532bfa74beb7065af34a11095d4bfd5ea9b
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-15 21:27:43 +0000

    C program that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/*
+ * Stucts, enums and unions definations
+ * Typedefs
+ * Functions prototypes
+ */
+
+#include <stdio.h>
+
+int _isupper(int c);
+
+#endif /* MAIN_H_ */
