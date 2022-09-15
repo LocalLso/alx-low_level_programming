bd22aecc37ce3882da0e8ea2d25a4b51cf68d3d4
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-15 14:02:02 +0000

    C program that prints out the largest of three numbers, no matter the case.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,39 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b || a < b) && (a > c || a < c) && (b > c || b < c))
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if ((b > a || a < b) && (b > c || b > c) && (a > c || a < c))
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else
+	largest = c;
+	if (a < b && a < c)
 	{
-		largest = c;
+		largest = a;
 	}
-
-	return (largest);
+	else if (b < a && b < c)
+	{
+		largest = b;
+	}
+	largest = c;
+	 if (a <= 0 && b >= 0 && c >= 0 && b > c)
+		largest = c;
+	else if (a <= 0 && b >= 0 && c >= 0 && c > b)
+		largest = c;
+	else if (a <=0 && b <= 0 && c >= 0)
+		largest = c;
+	else if (a <= 0 && b >= 0 && c <= 0)
+		largest = b;
+	else if (a <= 0 && b <= 0 && c >=0)
+		largest = c;
+	else if (a >= 0 && b <= 0 && c >= 0 && a > c)
+		largest = a;
+	else if (a >= 0 && b <= 0 && c >= 0 && c > a)
+		largest = c;
+	else if (a >= 0 && b <= 0 && c <= 0)
+		largest = a;
+	return(largest); 
 }
