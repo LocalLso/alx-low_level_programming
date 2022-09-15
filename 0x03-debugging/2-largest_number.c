82d1cca2aa1b4a7d61b830870636a3153f4ec9eb
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-15 10:48:38 +0000

    C program that prints out the largest of three numbers, no matter the case.

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: last number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if ((a > b || a < b) && (a > c || a < c) && (b > c || b < c))
+	{
+		largest = a;
+	}
+	else if (b > a && b > c && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest of 3 integers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+	int larg;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	larg = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", larg);
+
+	return (0);
+}
