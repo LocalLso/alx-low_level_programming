45744e35784328d0adacd67998f568b9e7fcf4fd
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-15 18:17:48 +0000

    C program that prints out the largest of three numbers, no matter the case.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,35 +12,12 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= 0 && b >= 0 && c >= 0 && a > b && b > c)
+	if (a >= b && b >= c)
 		largest = a;
-	else if (a >= 0 && b >= 0 && c >= 0 && b > a && b > c)
+	else if (b >= a && a >= c)
 		largest = b;
-	else if (a >= 0 && b >= 0 && c >= 0 && c > a && c > b)
+	else if (c >= a && a >= b)
 		largest = c;
-	else if (a <= 0 && b <= 0 && c <= 0 && a > b && a > c)
-		largest = a;
-	else if (a <= 0 && b <= 0 && c <= 0 && b > a && b > c)
-		largest = b;
-	else if (a <= 0 && b <= 0 && c <= 0 && c > a && c > b)
-		largest = c;
-	else if (a <= 0 && b >= 0 && c >= 0 && b > c)
-		largest = b;
-	else if (a <= 0 && b >= 0 && c >= 0 && c > b)
-		largest = c;
-	else if (a <= 0 && b <= 0 && c >= 0)
-		largest = c;
-	else if (a <= 0 && b >= 0 && c <= 0)
-		largest = b;
-	else if (a >= 0 && b >= 0 && c <= 0 && a > b)
-		largest = a;
-	else if (a >= 0 && b >= 0 && c <= 0 && b > a)
-		largest = b;
-	else if (a >= 0 && b <= 0 && c >= 0 && a > c)
-		largest = a;
-	else if (a >= 0 && b <= 0 && c >= 0 && c > a)
-		largest = c;
-	else if (a >= 0 && b <= 0 && c <= 0)
-		largest = a;
+
 	return (largest); 
 }
