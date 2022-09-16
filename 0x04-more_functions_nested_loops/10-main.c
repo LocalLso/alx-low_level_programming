31f8960aeb2e9133271b1220f0dd0f0812e5232c
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-16 18:53:49 +0000

    C program prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *@void: empty
+ *
+ * main - prints a triangle, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,30 @@
 #include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: integer to print
+ *
+ * print_triange -  prints a triangle, followed by a new line.
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+		if (j < ((size - 1) - i))
+		{
+		_putchar(' ');
+		}
+		else
+		{
+		_putchar('#');
+		}
+		}
+	_putchar('\n');
+	}
+}
