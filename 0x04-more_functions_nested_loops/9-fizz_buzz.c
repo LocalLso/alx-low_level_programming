dcf8a5b0e452abb6a4dd59cd391d2eab9e5e9b1c
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-16 17:13:16 +0000

    C program The “Fizz-Buzz test”.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,17 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if ((i % 3) == 0)
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz ");
-		}
-		else if (((i % 3) == 0) && ((i % 5) == 0))
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{
