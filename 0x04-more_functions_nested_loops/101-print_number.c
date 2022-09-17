14b3666825fa0004e900b8bf69bd2132efd0c012
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-17 02:37:48 +0000

    C program that prints an integer.

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ *
+ * print_number - a funtcion that prints an integer
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+	if ((n/10) != 0)
+	{
+		_putchar(n % 10);
+	}
+	else if (((n/10) == 0) && ((n%10) != 0) && (n > 0))
+	{
+		_putchar((n % 10) + '0');
+	}
+	else if (((n/10) == 0) && ((n%10) != 0) && (n <= 0))
+	{
+		_putchar((-n % 10) + '0');
+	}
+}
