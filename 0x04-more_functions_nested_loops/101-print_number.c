f1e648c6235b29171dce22e8a18224b85355ed60
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-17 02:56:20 +0000

    C program that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,13 +12,13 @@ void print_number(int n)
 {
 	if ((n / 10) != 0)
 	{
-		if(n > 0)
+		if (n > 0)
 		{
 			_putchar((n % 10) +'0');
 		}
 		else
 		{
-			_putchar(n % 10)
+			_putchar(n % 10);
 		}
 	}
 	else if (((n / 10) == 0) && ((n % 10) != 0) && (n > 0))
