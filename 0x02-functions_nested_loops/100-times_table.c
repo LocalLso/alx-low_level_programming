9ce475f6b4bb49cab5248d3810179150222336ee
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-18 15:44:53 +0000

    C program  that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,45 +11,39 @@ void print_times_table(int n)
 {
 	int i, j, mul;
 
-		for (i = 0; i <= n; i++)
-		{
-		for (j = 0; j <= n; j++)
-		{
-			mul = i * j;
-
-			if (j != 0)
-			{
-				_putchar(',');
-				if(mul > 100)
-				{
-					_putchar(' ');
-					continue;
-				}
-				_putchar(' ');
-				_putchar(' ');
-			}
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else if ((mul >= 10))
-			{
-				if (mul >= 100)
-				{
-					_putchar((mul / 100) + '0');
-					_putchar(((mul / 10) % 10) + '0');
-					_putchar((mul % 10) + '0');
-					continue;
-				}
-				_putchar((mul  / 10) + '0');
-				_putchar((mul % 10) + '0');
-			}
-			else if ((mul < 10) && (j != 0))
-			{
-				_putchar(' ');
-				_putchar((mul % 10) + '0');
-			}
-		}
-		_putchar('\n');
-		}
+	for (i = 0; i <= n; i++)
+	{
+	for (j = 0; j <= n; j++)
+	{
+		mul = i * j;
+	if (mul == 0)
+	{
+		_putchar('0');
+	}
+	if (mul < 10)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((mul % 10) + '0');
+	}
+	if ((mul >= 10) && (mul < 100))
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((mul / 10) + '0');
+		_putchar((mul % 10) + '0');
+	}
+	if (mul >= 100)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((mul / 100) + '0');
+		_putchar(((mul / 10) % 10) + '0');
+		_putchar((mul % 10) + '0');
+	}
+	}
+	}
 }
