6b9b0d1156bb33204e46e5e14e51aa5c9cfcdcd7
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-20 02:29:06 +0000

    C program that reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * puts2 - reverses string
+ * rev_string - reverses string
  * @s: pointeer of type char to print
  *
- * puts2 -  a function that reverses a string.
+ * rev_string -  a function that reverses a string.
  * Return: Always 0.
  */
 
@@ -15,7 +15,7 @@ void rev_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	;
-	
+
 	k = i;
 	for (i--, j = 0; j < k / 2; i--, j++)
 	{
