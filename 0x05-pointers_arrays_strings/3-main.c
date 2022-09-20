e6335511658208f702bcba2d197cfa91579e173a
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-20 01:52:24 +0000

    C program that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @str: pointer of char type to print
+ *
+ * main - a function that prints a string, followed by a new line, to stdout.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimo v";
+	_puts(str);
+	return (0);
+}
