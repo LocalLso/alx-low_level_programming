e72fd5a5b39e7502d73f971eb46adf8ff13f905e
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-23 01:56:10 +0000

    C program that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: pointer of char type
+ *
+ * rot13 - a function that encodes a string using rot13.
+ * Return: Always 0.
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[52] = {'a',
+		'b', 'c', 'd', 'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', 'A', 'B', 'C',
+		'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z'};
+	char o[52] = {'n', 'o', 'p', 'q', 'r',
+		's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f',
+		'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
+		'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J';
+		'K', 'L', 'M'}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = o[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
+char *rot13(char *s);
 
 
 #endif /* _MAIN_H_ */
