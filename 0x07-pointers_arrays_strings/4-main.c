2d8390ea5aa660002791e4d20e9e3a9adc3a5ec6
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-27 02:39:01 +0000

    C program that searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @void: empty
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strpbrk(s, f);
+	printf("%s\n", t);
+
+	return (0);
+}
