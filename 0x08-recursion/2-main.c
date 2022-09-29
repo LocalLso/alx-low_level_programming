6c537f7805a25a5042743189db81083eb0970a7a
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-29 02:43:15 +0000

    C program Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89  2-main.c 2-strlen_recursion.c -o 2-strlen_recursion

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @void: empty
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+
+	return (0);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: pointer of char type to print
+ *
+ * _strlen_recursion - a function that returns the length of a string.
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] != '\0')
+	{
+		i++;
+		return (i + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return(0);
+	}
+}
