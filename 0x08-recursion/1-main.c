4e0e0cb420571364d82cb4f29923a7d058fdec1b
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-29 02:03:09 +0000

    C program that prints a string in reverse.

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @void: empty
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	_print_rev_recursion("\nColton Walker");
+
+	return (0);
+}
