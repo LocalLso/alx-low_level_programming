b7c813b7b02472ab0a577ee6368d852e71b3dc29
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-09-30 23:01:00 +0000

    C static library libmy.a, my first static library.

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,15 +1,32 @@
 /*#include "mai.h"*/
+#include <stdio.h>
 
 /**
- * _strstr - prints string to stdout
- * @haystack: pointer of char type to print
- * @needle: pointer of char type to print
+ * _strstr - locates substring.
+ * @haystack: pointer of char type to print.
+ * @needle: pointer of char type to print.
  *
- * _strstr - a function that prints a string to stdout.
- * Return: haystack
+ * _strstr - a function that locates a substring.
+ * Return: a pointer to the located substring,
+ * or NULL if the substring is not found.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
+	char *startn = needle, *starth = haystack;
 
+	while (*haystack)
+	{
+		starth = haystack;
+		needle = startn;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
+			return (haystack);
+		haystack = starth + 1;
+	}
+	return (NULL);
 }
