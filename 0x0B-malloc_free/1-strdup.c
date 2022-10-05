ba23ef55f0313cff41000b2b450e11bd150b4073
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-10-05 02:05:03 +0000

    C program returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - finds the length of the string
+ * @s: pointer of char type to print
+ *
+ *_strlen _ finds the length of a given string
+ *Return: length.
+ */
+
+int _strlen(char *s)
+{
+	int i, len;
+
+	len = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len += s[i];
+	}
+	return (len);
+}
+
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: pointer of char type to print
+ *
+ * _strdup -  returns a pointer to a newly allocated space in memory.
+ * Return: NULL
+ * or a pointer.
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int size;
+	char *d;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = _strlen(str);
+	d = (char *)malloc(size * sizeof(char));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(d + i) = *(str + i);
+	}
+	*(d + i) = '\0';
+	return (d);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,6 +7,7 @@
  */
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 
 #endif /* _MAIN_H_ */
