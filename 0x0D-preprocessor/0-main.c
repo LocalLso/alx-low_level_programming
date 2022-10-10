29beec113df6fc4ac519732a87cf60f288b2d4b9
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-10-10 18:52:01 +0000

    C program that defines a macro named SIZE as an abbreviation for the token 1024.

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+
+/**
+ * main -check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
