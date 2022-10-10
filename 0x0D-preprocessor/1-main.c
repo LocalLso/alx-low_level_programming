8b1a8d50db4bd874a1eca00be05b1d0a753afc84
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-10-10 19:08:44 +0000

    C program  that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "1-pi.h"
+#include "1-pi.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
+	return (0);
+}
