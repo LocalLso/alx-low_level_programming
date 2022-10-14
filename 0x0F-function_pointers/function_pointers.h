51178f6394f595646471343b5c7862cc55ad54b9
LocalLso <106114567+LocalLso@users.noreply.github.com>
2022-10-15 01:07:29 +0200

    C iterator.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -16,5 +16,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
-
 #endif /* FUNCTION_POINTERS_H_ */
