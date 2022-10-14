9959fd71f31aeedc75fa4ac15a648af45d21c0d0
LocalLso <106114567+LocalLso@users.noreply.github.com>
2022-10-15 01:00:24 +0200

    C main update.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,26 +1,30 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * main - check the code for Holberton School students.
- * @argc: amount of args
+ * main - function
+ * @argc: size
  * @argv: arguments
- * Return: 1 if an error occurred, 0 otherwise
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	int result;
-	int a, b;
+	int (*j)(int, int);
+	int c, d;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	result = (*get_op_func(argv[2]))(a, b);
-	printf("%d\n", result);
+	j = get_op_func(argv[2]);
+	if (j == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = (atoi(argv[1]));
+	d = (atoi(argv[3]));
+	printf("%d\n", get_op_func(argv[2])(c, d));
 	return (0);
 }
