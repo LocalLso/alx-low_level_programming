f9cf40353a4a0d139a902c0859d9223dbdb4c9ed
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-10-14 02:16:16 +0000

    C program that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
@@ -9,5 +10,44 @@
 
 void print_all(const char * const format, ...)
 {
-	
+	char *str;
+	unsigned int i = 0;
+	va_list all;
+
+	va_start(all, format);
+	while (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", (char)va_arg(all, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(all, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(all, double));
+				break;
+			case 's':
+				str = va_arg(all, char *);
+				if (str != NULL)
+				{
+				printf("%s", str);
+				break;
+				}
+				printf("(nil)");
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 's' ||
+			format[i] == 'f') && (format[i + 1] != '\0'))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(all);
 }
