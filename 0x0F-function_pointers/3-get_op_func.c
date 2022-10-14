c0f07d4d01ba09ab81d0b9d8d48cd5607d4faae3
LocalLso <106114567+LocalLso@users.noreply.github.com>
2022-10-15 00:58:34 +0200

    C function that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,33 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * get_op_func - select correct function to perform operation
- *@s: operator passed as argument to the program
- *Return: a pointer to the function to the operator given
- *as a parameter
+ * get_op_func - function
+ * @s: operator
+ * Return: NULL if s not match any operators
  */
 int (*get_op_func(char *s))(int, int)
-{ /*array of structure of op_t and it has 2 members*/
- /*char *op and int *f (int, int)*/
+{
 	op_t ops[] = {
-	/*0*/	{"+", op_add}, /*+ is an string */
-	/*1*/	{"-", op_sub},
-	/*2*/	{"*", op_mul},
-	/*3*/	{"/", op_div},
-	/*4*/	{"%", op_mod},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (i < 5)
+	i = 0;
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-	i++;
+		i++;
 	}
-	printf("Error\n");
-	exit(99);
-}i
-/*function that will return a pointer a una funcion que recibe dos parametros y esa funcion me retorna un int*/
+	return (0);
+}
