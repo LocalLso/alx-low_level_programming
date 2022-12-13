99e01881566a90db166ca6b3015dfc0d8a77c36c
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-12-13 11:38:28 +0000

    C code injection.

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/LocalLso/alx-low_level_programming/blob/master/0x18-dynamic_libraries/code_injection.so
+wget -P /tmp https://github.com/LocalLso/alx-low_level_programming/blob/7ba3540438f68f2d43ef071913318e50d216a736/0x18-dynamic_libraries/code_injection.so
 export LD_PRELOAD=/tmp/code_injection.so
