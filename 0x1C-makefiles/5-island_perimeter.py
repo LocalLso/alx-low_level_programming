030d2dac3531d4310ea910d40652bf9c351ac2c8
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-02-25 02:47:47 +0000

    Python function def island_perimeter(grid): that returns the perimeter of the island described in grid.

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,4 +23,3 @@ def island_perimeter(grid: list) -> int:
                 bottom = 1 if i == rows - 1 or grid[i + 1][j] == 0 else 0
                 perimeter += (top + right + bottom + left)
     return perimeter
-
